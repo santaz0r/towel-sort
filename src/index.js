b3f407cdf3520e5af023cfdcf72e5cd16625aa0c
VictorM <neuron96@mail.ru>
2022-07-10 12:43:07 +0400

    towel-sort-task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    const newArr = [];
+
+    if (matrix) {
+        matrix.map((i, index) =>
+            index % 2 === 0
+                ? newArr.push(...i)
+                : newArr.push(...i.sort((a, b) => b - a))
+        );
+        return newArr;
+    } else {
+        return [];
+    }
+};
